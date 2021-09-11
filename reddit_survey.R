18693e09e01102e108c61db8a5e9e7b4c09089a6
Chidimma Echebiri <phoebechidimma@yahoo.com>
2021-09-11 16:22:45 +0200

    Reddit_survey

---
+++ reddit_survey.R
@@ -0,0 +1,15 @@
+
+reddit_survey1 <-read.csv("reddit.csv")
+#checking several parameters in data
+
+dim(reddit_survey1)
+#32754 columns and 12 rows
+
+#visualing based on income range
+library(ggplot2)
+table(reddit_survey1$income.range)
+reddit_survey1$income.range <-ordered(reddit_survey1$income.range, levels = c("Under $20,000", "$20,000 - $29,999","$30,000 - $39,999 ","$40,000 - $49,999" , "$50,000 - $69,999", "$70,000 - $99,999","$100,000 - $149,999 ","$150,000 or more"    ))
+qplot(data = reddit_survey1, x = income.range )
+
+table(reddit_survey1$employment.status)
+qplot(data = reddit_survey1, x = employment.status)
