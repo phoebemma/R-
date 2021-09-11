f048285792a028ad8b8e98a5c95194e841b7a7b9
Chidimma Echebiri <phoebechidimma@yahoo.com>
2021-09-11 22:33:41 +0200

    Add files via upload

--- Heart failure prediction dataset.R
+++ Heart failure prediction dataset.R
@@ -44,7 +44,7 @@ heart_failure_prediction_dataset %>%
   select(ChestPainType, ends_with("RestingBP")) %>%
   group_by(ChestPainType) %>% 
   summarise(across(everything(), list(mean = mean, median = median,  sd = sd)))
-
+#visualisation using boxplots
 options(repr.plot.width=12, repr.plot.height=12)
 fig1 <- ggplot(heart_failure_prediction_dataset, aes(y=Age))+geom_boxplot()
 fig2 <- ggplot(heart_failure_prediction_dataset, aes(y=ChestPainType))+geom_boxplot()
@@ -63,3 +63,23 @@ grid.arrange(fig1,fig2,fig3,fig4,fig5,fig6,fig7,fig8,fig9,fig10,fig11,fig12, nco
 
 m<-cor(heart_failure_prediction_dataset, x  = Cholesterol, y = RestingBP)
 corrplot(m, method="number")
+
+#visualization using histogram
+options()
+stat_bin(bins = 12)
+fig1 <- ggplot(heart_failure_prediction_dataset, aes(y=Age))+geom_histogram()
+fig2 <- ggplot(heart_failure_prediction_dataset, aes(y=ChestPainType))+geom_histogram()
+fig3 <- ggplot(heart_failure_prediction_dataset, aes(y=Sex))+geom_histogram()
+fig4 <- ggplot(heart_failure_prediction_dataset, aes(y=RestingECG))+geom_histogram()
+fig5 <- ggplot(heart_failure_prediction_dataset, aes(y=FastingBS))+geom_histogram()
+fig6 <- ggplot(heart_failure_prediction_dataset, aes(y=MaxHR))+geom_histogram()
+fig7 <- ggplot(heart_failure_prediction_dataset, aes(y=ExerciseAngina))+geom_histogram()
+fig8 <- ggplot(heart_failure_prediction_dataset, aes(y=ST_Slope))+geom_histogram()
+fig9 <- ggplot(heart_failure_prediction_dataset, aes(y=HeartDisease))+geom_histogram()
+fig10 <- ggplot(heart_failure_prediction_dataset, aes(y=Oldpeak))+geom_histogram()
+fig11 <- ggplot(heart_failure_prediction_dataset, aes(y=Cholesterol))+geom_histogram()
+fig12 <- ggplot(heart_failure_prediction_dataset, aes(y=RestingBP))+geom_histogram()
+
+grid.arrange(fig1,fig2,fig3,fig4,fig5,fig6,fig7,fig8,fig9,fig10,fig11,fig12, ncol=4, nrow=4)
+
+
