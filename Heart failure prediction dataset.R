04519cc3db261d3a994b90fddae5bb3511628de3
Chidimma Echebiri <phoebechidimma@yahoo.com>
2021-09-11 22:18:21 +0200

    Add files via upload

--- Heart failure prediction dataset.R
+++ Heart failure prediction dataset.R
@@ -2,6 +2,8 @@ install.packages("caret")
 install.packages("randomForest")
 library(caret)
 library(randomForest)
+library(gridExtra)
+library(corrplot)
 #Retrieving the column names/titles
 colnames(heart_failure_prediction_dataset)
 str(heart_failure_prediction_dataset)
@@ -42,3 +44,22 @@ heart_failure_prediction_dataset %>%
   select(ChestPainType, ends_with("RestingBP")) %>%
   group_by(ChestPainType) %>% 
   summarise(across(everything(), list(mean = mean, median = median,  sd = sd)))
+
+options(repr.plot.width=12, repr.plot.height=12)
+fig1 <- ggplot(heart_failure_prediction_dataset, aes(y=Age))+geom_boxplot()
+fig2 <- ggplot(heart_failure_prediction_dataset, aes(y=ChestPainType))+geom_boxplot()
+fig3 <- ggplot(heart_failure_prediction_dataset, aes(y=Sex))+geom_boxplot()
+fig4 <- ggplot(heart_failure_prediction_dataset, aes(y=RestingECG))+geom_boxplot()
+fig5 <- ggplot(heart_failure_prediction_dataset, aes(y=FastingBS))+geom_boxplot()
+fig6 <- ggplot(heart_failure_prediction_dataset, aes(y=MaxHR))+geom_boxplot()
+fig7 <- ggplot(heart_failure_prediction_dataset, aes(y=ExerciseAngina))+geom_boxplot()
+fig8 <- ggplot(heart_failure_prediction_dataset, aes(y=ST_Slope))+geom_boxplot()
+fig9 <- ggplot(heart_failure_prediction_dataset, aes(y=HeartDisease))+geom_boxplot()
+fig10 <- ggplot(heart_failure_prediction_dataset, aes(y=Oldpeak))+geom_boxplot()
+fig11 <- ggplot(heart_failure_prediction_dataset, aes(y=Cholesterol))+geom_boxplot()
+fig12 <- ggplot(heart_failure_prediction_dataset, aes(y=RestingBP))+geom_boxplot()
+
+grid.arrange(fig1,fig2,fig3,fig4,fig5,fig6,fig7,fig8,fig9,fig10,fig11,fig12, ncol=4, nrow=4)
+
+m<-cor(heart_failure_prediction_dataset, x  = Cholesterol, y = RestingBP)
+corrplot(m, method="number")
